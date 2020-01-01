4c9bfc38dfc01c0c50bd6c9308881909f7d67186
venpong <venpong.x@gmail.com>
2020-01-01 22:57:10 +0800

    fix lan ip

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 #echo '修改机器名称'
 #sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
