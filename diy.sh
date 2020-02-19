25ca5561ffb9e8435233cf6473f3cf2c4bcece5b
venpong <venpong.x@gmail.com>
2020-02-19 11:33:58 +0800

    fix serverchan

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@
 #sed -i 's/OpenWrt/G-DOCK/g' package/base-files/files/bin/config_generate
 
 #echo '修改网关地址'
-#sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
 #echo '修改时区'
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -34,10 +34,10 @@ sed -i 's/OpenWrt/PISEN/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #cp -f ../banner package/base-files/files/etc/
 
 #echo '下载ServerChan'
-#git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
 
 #echo '集成diy目录'
-#ln -s ../../diy ./package/openwrt-packages
+ln -s ../../diy ./package/openwrt-packages
 
 
 
