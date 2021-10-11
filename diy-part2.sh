6deea545eb26adc57464160137ececb7cd102416
venpong <venpong@163.com>
2021-10-11 17:05:07 +0800

    auto sync

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,8 +36,8 @@ sed -i 's/OpenWrt/ASUS/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #rm -rf package/base-files/files/etc/banner
 #cp -f ../banner package/base-files/files/etc/
 
-#echo '下载ServerChan'
-git clone https://github.com/tty228/luci-app-serverchan ../diy/luci-app-serverchan
+#echo '下载PushBot'
+git clone -b stable https://github.com/venpong/luci-app-pushbot.git ../diy/luci-app-pushbot
 
 #echo '集成diy目录'
 ln -s ../../diy ./package/openwrt-packages
