048f46f1fce998e656b0b40ff0e1d534927cb89e
venpong <venpong.x@gmail.com>
2022-05-08 21:04:07 +0800

    fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -43,7 +43,7 @@
 #git clone -b stable https://github.com/venpong/luci-app-pushbot.git ../diy/luci-app-pushbot
 
 #echo '集成diy目录'
-ln -s ../../diy ./package/openwrt-packages
+#ln -s ../../diy ./package/openwrt-packages
 
 
 
